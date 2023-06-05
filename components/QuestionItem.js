f6325c5cbbe90094130a230bcc2eb4d042192c3b
akash <akashjnv007@gmail.com>
2023-06-05 10:54:22 +0530

    jdk

--- components/QuestionItem.js
+++ components/QuestionItem.js
@@ -3,9 +3,9 @@ import  sanityClient from '@sanity/client'
 
 
 const client = sanityClient({
-  projectId: 'j856t9gr',
+  projectId: '1xrz0qzi',
   dataset: 'production',
-  apiVersion: '2023-01-30', // use current UTC date - see "specifying API version"!
+  apiVersion: '2023-06-05', // use current UTC date - see "specifying API version"!
    // or leave blank for unauthenticated usage
   useCdn: true,  // `false` if you want to ensure fresh data
 })
